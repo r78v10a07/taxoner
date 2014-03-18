8024049d9a2af033300b47c1f6955d2409dc751f
r78v10a07@gmail.com <r78v10a07@gmail.com@848d4233-5045-7d02-6516-10a7a862f914>
2014-03-18 15:36:21 +0000

    Adding databases pages

--- trunk/Taxoner/app/js/app.js
+++ trunk/Taxoner/app/js/app.js
@@ -12,8 +12,10 @@ angular.module('myApp', [
         config(['$routeProvider', function($routeProvider) {
                 $routeProvider.when('/introduction', {templateUrl: 'partials/introduction.html', controller: 'MyCtrl1'});
                 $routeProvider.when('/taxform', {templateUrl: 'partials/taxform.html', controller: 'MyCtrl1'});
+                $routeProvider.when('/datainfo', {templateUrl: 'partials/datainfo.html', controller: 'MyCtrl1'});
                 $routeProvider.when('/datacreator', {templateUrl: 'partials/datacreator.html', controller: 'MyCtrl1'});
                 $routeProvider.when('/geneassignment', {templateUrl: 'partials/geneassignment.html', controller: 'MyCtrl1'});
+                $routeProvider.when('/genedatainfo', {templateUrl: 'partials/genedatainfo.html', controller: 'MyCtrl1'});
                 $routeProvider.when('/server_crew', {templateUrl: 'partials/server_crew.html', controller: 'MyCtrl1'});
                 $routeProvider.otherwise({redirectTo: '/introduction'});
             }]);
