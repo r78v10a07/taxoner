43d6e8d78a992dd61c6e9f275d2dc0015b5ad892
r78v10a07@gmail.com <r78v10a07@gmail.com@848d4233-5045-7d02-6516-10a7a862f914>
2014-03-20 15:14:28 +0000

    Adding correct path into the databases folder

--- trunk/Taxoner/bin/dbDownloader.sh
+++ trunk/Taxoner/bin/dbDownloader.sh
@@ -4,9 +4,10 @@ if [ -e "databases" ]
 then
    cd databases
    wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip
-   unzip -d ./ taxdmp.zip
-   cd geneassignment
-   wget -np -nH -r --cut-dirs=1 http://pongor.itk.ppke.hu/~roberto/geneassigment/
+   unzip -d ./ taxdmp.zip nodes.dmp
+   rm taxdmp.zip
+   wget -r --reject "index.html*" -e robots=off -np -nH --cut-dirs=1 http://pongor.itk.ppke.hu/~roberto/geneassignment/   
+   wget -r --reject "index.html*" -e robots=off -np -nH --cut-dirs=2 http://pongor.itk.ppke.hu/~roberto/taxoner/bowtie2/
 else
    echo "Please, go to the Taxoner folder"
 fi
