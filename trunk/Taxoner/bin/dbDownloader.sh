002eda1335c46a7391a5785ff574a11827540381
r78v10a07@gmail.com <r78v10a07@gmail.com@848d4233-5045-7d02-6516-10a7a862f914>
2014-03-26 19:57:58 +0000

    Adding a summary server to the Node.JS script and the frame to show the summary in the main site

--- trunk/Taxoner/bin/dbDownloader.sh
+++ trunk/Taxoner/bin/dbDownloader.sh
@@ -4,8 +4,9 @@ if [ -e "databases" ]
 then
    cd databases
    wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip
-   unzip -d ./ taxdmp.zip nodes.dmp
-   rm taxdmp.zip
+   unzip -d ./ taxdmp.zip nodes.dmp names.dmp
+   grep "scientific name" names.dmp | awk -F\\t\|\\t '{printf("%s\t%s\n",$1,$3)}' > names
+   rm taxdmp.zip names.dmp
    wget -r --reject "index.html*" -e robots=off -np -nH --cut-dirs=1 http://pongor.itk.ppke.hu/~roberto/geneassignment/   
    wget -r --reject "index.html*" -e robots=off -np -nH --cut-dirs=2 http://pongor.itk.ppke.hu/~roberto/taxoner/bowtie2/
 else
