bf41fda46661b4fdbbe0e91de161042809ad731d
r78v10a07@gmail.com <r78v10a07@gmail.com@848d4233-5045-7d02-6516-10a7a862f914>
2014-03-28 15:22:10 +0000

    Adding a summary form

--- Taxoner/app/js/app.js
+++ Taxoner/app/js/app.js
@@ -16,6 +16,7 @@ angular.module('myApp', [
                 $routeProvider.when('/datacreator', {templateUrl: 'partials/datacreator.html', controller: 'MyCtrl1'});
                 $routeProvider.when('/geneassignment', {templateUrl: 'partials/geneassignment.html', controller: 'MyCtrl1'});
                 $routeProvider.when('/genedatainfo', {templateUrl: 'partials/genedatainfo.html', controller: 'MyCtrl1'});
+                $routeProvider.when('/summaryform', {templateUrl: 'partials/summaryform.xhtml', controller: 'MyCtrl1'});
                 $routeProvider.when('/server_crew', {templateUrl: 'partials/server_crew.html', controller: 'MyCtrl1'});
                 $routeProvider.otherwise({redirectTo: '/introduction'});
             }]);
