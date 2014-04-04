b87c5297433de595ea505f3051a2df1487f89036
r78v10a07@gmail.com <r78v10a07@gmail.com@848d4233-5045-7d02-6516-10a7a862f914>
2014-04-04 08:39:44 +0000

    Changing the summary form

--- trunk/Taxoner/server.js
+++ trunk/Taxoner/server.js
@@ -313,23 +313,27 @@ function server(filename, tax, res) {
                                 for (var i = 0; i < sortable.length; i++) {
                                     var key = sortable[i][0];
                                     var taxId = taxs[key]['taxId'];
-                                    result = result
-                                            + "<tr><td>"
-                                            + "<a href='https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="
-                                            + taxId
-                                            + "' target='_blank'>"
-                                            + nodesMap[taxId]['name']
-                                            + " ("
-                                            + taxId
-                                            + ")"
-                                            + "</a>"
-                                            + "</td><td>"
-                                            + nodesMap[taxId]['rank']
-                                            + "</td><td>"
-                                            + taxs[taxId]['prot']
-                                            + "</td><td>"
-                                            + taxs[taxId]['total']
-                                            + "</td></tr>";
+                                    if (typeof (nodesMap[taxId]) === "undefined") {
+                                        console.log("TaxId: ", taxId);
+                                    } else {
+                                        result = result
+                                                + "<tr><td>"
+                                                + "<a href='https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="
+                                                + taxId
+                                                + "' target='_blank'>"
+                                                + nodesMap[taxId]['name']
+                                                + " ("
+                                                + taxId
+                                                + ")"
+                                                + "</a>"
+                                                + "</td><td>"
+                                                + nodesMap[taxId]['rank']
+                                                + "</td><td>"
+                                                + taxs[taxId]['prot']
+                                                + "</td><td>"
+                                                + taxs[taxId]['total']
+                                                + "</td></tr>";
+                                    }
                                 }
                                 result = result
                                         + "</table>";
