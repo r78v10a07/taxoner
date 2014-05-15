e46bfb9204431c53f9f77e98b7145c9b65c31719
r78v10a07@gmail.com <r78v10a07@gmail.com@848d4233-5045-7d02-6516-10a7a862f914>
2014-05-15 14:26:35 +0000

    Refactoring fasta code to rename functions

--- trunk/Taxoner/bin/dbDownloader.sh
+++ trunk/Taxoner/bin/dbDownloader.sh
@@ -7,8 +7,8 @@ then
    unzip -d ./ taxdmp.zip nodes.dmp names.dmp
    grep "scientific name" names.dmp | awk -F\\t\|\\t '{printf("%s\t%s\n",$1,$3)}' > names
    rm taxdmp.zip names.dmp
-   wget -r --reject "index.html*" -e robots=off -np -nH --cut-dirs=1 http://pongor.itk.ppke.hu/~roberto/geneassignment/   
-   wget -r --reject "index.html*" -e robots=off -np -nH --cut-dirs=2 http://pongor.itk.ppke.hu/~roberto/taxoner/bowtie2/
+   wget -r --reject "index.html*" -e robots=off -np -nH --cut-dirs=1 http://pongor.itk.ppke.hu/taxoner/databases/geneassignment/   
+   wget -r --reject "index.html*" -e robots=off -np -nH --cut-dirs=2 http://pongor.itk.ppke.hu/taxoner/databases/bowtie2/
 else
    echo "Please, go to the Taxoner folder"
 fi
