3967e19c9e8c559e68e8f302d9eb8b350c1b6211
pongorlorinc@gmail.com <pongorlorinc@gmail.com@848d4233-5045-7d02-6516-10a7a862f914>
2014-08-15 09:50:46 +0000

    Version 2 of dbcreator

---
+++ trunk/Taxoner/src/dbCreator2/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: lorinc
+ *
+ * Created on August 15, 2014, 11:46 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
